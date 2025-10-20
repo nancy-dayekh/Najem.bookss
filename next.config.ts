45d60eeea4346431a5ec79579993981b78ef1b38
Nancy Mohammad Dayekh <nancydayekh43@gmail.com>
2025-10-20 13:51:36 +0300

    edit navbar

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "jewkhodpkyqdwknnrxld.supabase.co",
-        port: "",
-        pathname: "/storage/v1/object/public/**", // ✅ allow all folders (logos, products, etc.)
+        protocol: 'https',
+        hostname: 'jewkhodpkyqdwknnrxld.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/products-images/**',
       },
     ],
   },
